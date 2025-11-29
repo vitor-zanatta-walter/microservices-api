f6955d8dcbc4fb865357dd38a01f9666530fd3f7
Vitor Zanatta Walter <vitorzanattawalter@gmail.com>
2025-11-28 22:32:30 -0300

    cpf nullable

--- user/controllers/UserController.js
+++ user/controllers/UserController.js
@@ -9,8 +9,8 @@ export default class UserController {
         try {
             const { name, cpf, email, password } = req.body;
 
-            if (!name || !cpf || !email || !password) {
-                return res.status(400).json({ error: 'name, cpf, email e password são obrigatórios' });
+            if (!name || !email || !password) {
+                return res.status(400).json({ error: 'name, email e password são obrigatórios' });
             }
 
             // verificar se já existe usuário com o mesmo email
@@ -34,8 +34,8 @@ export default class UserController {
         try {
             const { name, cpf, email, password } = req.body;
 
-            if (!name || !cpf || !email || !password) {
-                return res.status(400).json({ error: 'name, cpf, email e password são obrigatórios' });
+            if (!name || !email || !password) {
+                return res.status(400).json({ error: 'name, email e password são obrigatórios' });
             }
 
             const hashedPassword = await hashPassword(password);
@@ -55,7 +55,7 @@ export default class UserController {
 
             const user = await User.get(id);
             return res.status(200).json(UserView.render(user));
-        
+
         } catch (error) {
             console.error('Erro em UserController.getMyData', error);
             return res.status(500).json({ error: `Erro ao buscar dados do usuário` });
@@ -65,19 +65,19 @@ export default class UserController {
     // buscar usuário por ID
     static async get(req, res) {
         try {
-            
+
             // dados da query
             const searchId = req.params.user_id;
-            
+
             // dados do token
             const is_attendant = req.user.is_attendant;
             const selfId = req.user.id;
-            
+
             // se for procurar outro usuário, verificar permissão
-            if ( (searchId !== selfId) || !is_attendant) {
-                return res.status(403).json({ error: 'Permissão insuficiente' });            
+            if ((searchId !== selfId) || !is_attendant) {
+                return res.status(403).json({ error: 'Permissão insuficiente' });
             }
-            
+
             const user = await User.get(searchId);
             if (!user) return res.status(404).json({ error: 'Usuário não encontrado' });
 
@@ -101,7 +101,7 @@ export default class UserController {
 
             const user = await User.getByEmail(email);
             if (!user) return res.status(404).json({ error: 'Usuário não encontrado' });
-            
+
             return res.status(200).json(UserView.render(user));
         } catch (error) {
             console.error('Erro em UserController.getByEmail', error);
@@ -132,7 +132,7 @@ export default class UserController {
     // atualizar usuário
     static async update(req, res) {
         try {
-            const { id: selfId, is_attendant: iAmAttendant  } = req.user;
+            const { id: selfId, is_attendant: iAmAttendant } = req.user;
             const { name, email, password, cpf } = req.body;
             const { user_id: editingId } = req.params;
 
@@ -158,7 +158,7 @@ export default class UserController {
     // promover usuário a atendente
     static async promote(req, res) {
         try {
-            const { id: selfId, is_attendant: iAmAttendant  } = req.user;
+            const { id: selfId, is_attendant: iAmAttendant } = req.user;
             const { user_id: editingId } = req.params;
 
             // verificar permissões
@@ -178,7 +178,7 @@ export default class UserController {
     // rebaixar usuário
     static async demote(req, res) {
         try {
-            const { id: selfId, is_attendant: iAmAttendant  } = req.user;
+            const { id: selfId, is_attendant: iAmAttendant } = req.user;
             const { user_id: editingId } = req.params;
 
             // verificar permissões
