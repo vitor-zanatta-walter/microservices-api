830eb99dfa460cb5eef7a84a49be83c52712298e
Vitor Zanatta Walter <vitorzanattawalter@gmail.com>
2025-12-01 15:45:21 -0300

    refactor: merge migration into schema.sql

--- sql/migration_add_finished_to_events.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add finished column to Events table
--- This column tracks whether an event has been marked as finished
-
-ALTER TABLE `Events` 
-ADD COLUMN `finished` BOOLEAN NOT NULL DEFAULT 0;
